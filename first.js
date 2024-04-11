68ab987e6cc25ae4115c2b838d14984f30d9c141
grmmahato <grmmahato12@gmail.com>
2024-04-11 16:57:25 +0545

    first commit

---
+++ first.js
@@ -0,0 +1,20 @@
+fullName = "siddharth singh";
+console.log(fullName);
+
+age = 24;
+price = 99.33;
+console.log(price);
+
+x = null;
+console.log(x);
+
+y = undefined;
+
+console.log(y);
+
+isFollow = false;
+console.log(isFollow);
+
+FullName = "siddharth mahato";
+console.log(fullName);
+console.log(FullName);
