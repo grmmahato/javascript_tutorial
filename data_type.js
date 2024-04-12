85a03f7fd952e3f024c9f50129ce8012f634c944
grmmahato <grmmahato12@gmail.com>
2024-04-12 15:32:05 +0545

    variable const and let

---
+++ data_type.js
@@ -0,0 +1,29 @@
+// Data Type in JS
+// (This is Primitive Data types)
+// Number, String, Boolean, Underfined, Null, BigInt, Symbol
+
+// ***********************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+//**************************************************** */
+// Non-primitive Data Types 
+
+const student ={
+      fullName: "Sid",
+      age: 24,
+      cgpa: 8.5,
+      isPass: true
+};
+
+console.log(student);
