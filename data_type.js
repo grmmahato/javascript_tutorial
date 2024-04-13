7e838b2d9bab581af2374227998db1f59019fc67
grmmahato <grmmahato12@gmail.com>
2024-04-14 00:21:54 +0545

    primitive data types added

--- data_type.js
+++ data_type.js
@@ -3,6 +3,26 @@
 // Number, String, Boolean, Underfined, Null, BigInt, Symbol
 
 // ***********************************************************
+let a = 25; // number
+let price = 100.2
+let fullName = "sidharth singh " // string
+
+isFollow = true   // boolean
+
+let x;  // underfined
+let y = null; // null
+
+let b = BigInt("1234"); //  BigInt
+
+let c = Symbol("hello!"); // Symbol
+
+
+
+
+
+
+
+
 
 
 
